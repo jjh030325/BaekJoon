4f4b61567c119d8fa014194d8cee6bc8340a38ed
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:27:20 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    
+    cin>>a;
+    
+    for(int i=0;i<a;i++)
+    {
+        cin>>b>>c;
+        cout<<b+c<<endl;
+    }
+    
+    return 0;
+}
