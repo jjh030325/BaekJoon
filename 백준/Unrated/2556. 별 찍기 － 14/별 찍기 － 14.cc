614fca9c8fde23396f6d4b94c727d088e6ee9a11
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:17:01 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+
+	scanf("%d", &x);
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < x; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
