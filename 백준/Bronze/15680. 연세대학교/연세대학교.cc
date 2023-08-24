558cea7e0a85e9d53f6e3d633d6612271bd9cf3e
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:32:47 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if(a==0)
+    {
+        cout<<"YONSEI"<<endl;
+    }else
+    {
+        cout<<"Leading the Way to the Future"<<endl;
+    }
+    return 0;
+}
