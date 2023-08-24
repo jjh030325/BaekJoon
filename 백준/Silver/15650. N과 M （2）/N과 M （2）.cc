3465410bd2714fb98088322264e201ea239f6a7b
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:32:03 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9];
+
+void dps(int a, int len)
+{
+	arr[len] = a;
+	if (len == m)
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	len++;
+	for (int i = a + 1; i <= n; i++)
+	{
+		dps(i, len);
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		dps(i, 1);
+	}
+	return 0;
+}
