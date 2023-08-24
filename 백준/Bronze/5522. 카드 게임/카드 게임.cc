0d47f49411259c3fec139405efec13fe688be247
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:22:09 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int dap = 0;
+    for(int i=0;i<5;i++)
+    {
+        cin>>a;
+        dap+=a;
+    }
+    cout<<dap<<endl;
+    return 0;
+}
