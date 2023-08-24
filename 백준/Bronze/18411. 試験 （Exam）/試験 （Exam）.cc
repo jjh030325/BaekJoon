d93c2cb32935a002a1ee4891cbe295e1197972f7
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:35:26 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a[3];
+    for(int i=0;i<3;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a, a+3);
+    cout<<a[1]+a[2]<<endl;
+    return 0;
+}
