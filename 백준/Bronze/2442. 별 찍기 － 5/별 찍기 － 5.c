443ae98fbb677723895c7fb1a0fc258e30761f99
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:15:25 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int y, x;
+	
+	scanf("%d", &x);
+	y = x * 2 + 1;
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			if (x - i - 1 > j)
+			{
+				printf(" ");
+			}
+			else if (x + i - 1 < j)
+			{
+				continue;
+			}
+			else
+			{
+				printf("*");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
