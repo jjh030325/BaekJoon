2dfbeb135fc439936a9daa038e29d8c2b75aef0e
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:16:04 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int max = 0, people = 0, x, y;
+
+	for (int i = 0; i < 10; i++)
+	{
+		scanf("%d %d", &x, &y);
+		people -= x;
+		people += y;
+		if (people > max)
+		{
+			max = people;
+		}
+	}
+	printf("%d", max);
+
+	return 0;
+}
