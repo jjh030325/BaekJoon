e66b91c24e2ff73a5ae0a8ec7ffe25c53a282951
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:19:10 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    
+    cin>>n;
+    
+    for(int i=n;i>=1;i--)
+    {
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
