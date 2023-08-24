4efa525306757ef46a395125db88ed787dc405ed
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:35:39 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==1)
+    {
+        cout<<"SciComLove"<<endl;
+    }else
+    {
+        cout<<"SciComLove"<<endl;
+        cout<<"SciComLove"<<endl;
+    }
+    return 0;
+}
