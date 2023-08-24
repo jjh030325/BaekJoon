cc1394290a3cb7eba2032bfd812e911a6e85b0c6
jjh030325 <117177784+jjh030325@users.noreply.github.com>
2023-08-25 02:37:49 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    if(n*100>=m)
+    {
+        cout<<"Yes"<<endl;
+    }else
+    {
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
